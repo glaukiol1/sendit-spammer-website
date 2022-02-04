38d9ce32e3ec2fd2d0e5fda9bac400f5f5a76cf7
glaukiol1 <glaukiol1@gmail.com>
2022-02-04 22:09:03 +0100

    finished

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,43 @@
 
 console.log("Hello From JavaScript File!")
 
+const headers = {
+    'Host': 'api.getsendit.com',
+    'Content-Length': '354',
+    'Sec-Ch-Ua': '" Not A;Brand";v="99", "Chromium";v="92"',
+    'App-Id': 'c2ad997f-1bf2-4f2c-b5fd-83926e8f3c65',
+    'App-Version': '1.0',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Content-Type': 'application/json',
+    'Accept': '*/*',
+    'Origin': 'https://web.getsendit.com',
+    'Sec-Fetch-Site': 'same-site',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://web.getsendit.com/',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+
+document.getElementById('btn').addEventListener('click', (e) => {
+    fetch('/senditid?url=' + document.getElementById('url').value + '&question=' + document.getElementById('question').value)
+        .then(resp => resp.json())
+        .then(json => {
+            for (let i = 0; i < 100; i++) {
+                fetch('https://api.getsendit.com/v1/posts', {
+                    headers,
+                    method: 'POST',
+                    body: JSON.stringify(json)
+                })
+                    .then(r => {
+                        document.getElementById('status').innerHTML += "<br>"+r.status+" SUCCESS"
+                    })
+                    .catch(err => {
+                        document.getElementById('status').innerHTML += "<br>"+err+" ERROR"
+                    })
+            }
+            
+        })
+
+})
+
--- server.js
+++ server.js
@@ -15,9 +15,45 @@ app.set('view engine', 'pug') // Use the pug langauge to default view
 app.use(express.static('public')) // Set the public folder to be static in our server
 
 
-app.get('/', (req,res) => { // Setup GET request on the / route
+app.get('/', function (req, res) { // Setup GET request on the / route
 	res.render('index.pug') // Render the index.pug file to the client
 })
 
-app.listen(3000, ()=>{console.log('Server Started on 127.0.0.1:3000!')}) // Listen on port 3000
+app.get('/senditid', async (req, res) => {
+	var webdriver = require("selenium-webdriver");
+
+	var browser = new webdriver.Builder().forBrowser('chrome').build();
+	await browser.get(req.query.url);
+
+	var promise = browser.getTitle();
+
+	promise.then(function (title) {
+		console.log(title);
+	});
+	browser.executeScript(`
+	document.getElementsByTagName('noscript')[0].id = 'data-div-1234'
+	globalThis.fetch = (url,init) => {
+		document.getElementById('data-div-1234').innerHTML = init.body
+		return fetch(url,init)
+	}
+	`)
+	setTimeout(() => {
+		browser.findElement(webdriver.By.className('input-textbox')).sendKeys(req.query.question).then( () => {
+			browser.findElement(webdriver.By.className('send-button')).click().then(()=>{
+				setTimeout( () => {
+					browser.executeScript(function() {
+						return document.querySelector('#data-div-1234').innerHTML;
+					  }).then(function(innerHTML) {
+						res.json(JSON.parse(innerHTML)).status(200)
+					  });
+				}, 1000)
+				
+			})
+		})
+		
+	}, 1000)
+	
+})
+
+app.listen(3000, () => { console.log('Server Started on 127.0.0.1:3000!') }) // Listen on port 3000
 
